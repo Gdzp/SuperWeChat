0e5da65ba5d1ea8c2f93e6341e4369da7a71feb4
zhouhuiping <email@example.com>
2016-11-01 20:52:05 +0800

    添加跳转和Login的工具类

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -2,8 +2,12 @@ package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.img_login, R.id.img_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_login:
+                MFGT.gotoLogin(this);
+                break;
+            case R.id.img_register:
+                MFGT.gotoRegister(this);
+                break;
+        }
     }
 }
